f26f0d9d5b36629f2f7e8e7cfd9e2f4705662748
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-08-15 01:01:33 +0300

    add enter button listener

--- src/lib/TodoAddItem/component.svelte
+++ src/lib/TodoAddItem/component.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   export let todoText: string = "";
   export let onAddHandler: (todoText: string) => void;
+
+  function handleKeydown(e: KeyboardEvent) {
+    // add task if "enter" pressed
+    if (e && e.key === "Enter" && todoText && todoText.length > 0) {
+      onAddHandler(todoText);
+    }
+  }
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <div class="todoCard__addNewItem">
   <input
